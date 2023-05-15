3ce89bab5aaa825287bfd4e11d0466d2866b9b95
fabianhinz <hinzfabian.fh@gmail.com>
2023-05-15 15:06:47 +0200

    test parse context

--- automagicallyexecute/index.ts
+++ automagicallyexecute/index.ts
@@ -1,28 +1,48 @@
-import tl from 'azure-pipelines-task-lib'
-
+import { TaskResult, getInputRequired, getVariable, setResult, getEndpointAuthorizationParameter } from 'azure-pipelines-task-lib'
 import fetch from 'node-fetch'
 
 const AUTOMAGICALLY_BASE_URL = 'https://automagically-5vr3ysri3a-ey.a.run.app/'
 
 const run = async (): Promise<void> => {
   try {
-    const url = tl.getInputRequired('url')
+    const url = getInputRequired('url')
     if (!url) {
-      tl.setResult(tl.TaskResult.Failed, 'URL is required')
+      setResult(TaskResult.Failed, 'URL is required')
       return
     }
 
-    const token = tl.getInputRequired('token')
+    const token = getInputRequired('token')
     if (!token) {
-      tl.setResult(tl.TaskResult.Failed, 'token is required')
+      setResult(TaskResult.Failed, 'token is required')
       return
     }
+
+    // https://github.com/microsoft/azure-pipelines-task-lib/issues/579
+    const accessToken = getEndpointAuthorizationParameter("SystemVssConnection", "AccessToken", false);
+    
+    // https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables
+    const collectionUri = getVariable('System.TeamFoundationCollectionUri');
+    const organization = collectionUri ? new URL(collectionUri).pathname.split("/").at(1) : undefined;
+    const project = getVariable('System.TeamProject')
+    const repositoryId = getVariable('Build.Repository.ID')
+    const pullRequestId = getVariable('System.PullRequest.PullRequestId')
 
     const response = await fetch(`${AUTOMAGICALLY_BASE_URL}/api/v1/execute`, {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({token, url, context: {source: 'azureDevOps'}}),
+      body: JSON.stringify({
+        token,
+        url,
+        context: {
+          source: 'azureDevOps',
+          accessToken,
+          organization,
+          project,
+          repositoryId,
+          pullRequestId,
+        }
+      }),
       method: 'POST'
     })
 
@@ -33,8 +53,8 @@ const run = async (): Promise<void> => {
       )
     }
   } catch (err) {
-    tl.setResult(
-      tl.TaskResult.Failed,
+    setResult(
+      TaskResult.Failed,
       err &&
         typeof err === 'object' &&
         'message' in err &&
